d2221f5a2805023bbff2a1f2335f25f80a50338a
Hudson Conceição Da Silva <hud90cs@gmail.com>
2015-09-04 20:24:41 -0300

    Iniciando o projeto

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from setuptools import find_packages, setup
+
+setup(
+    name='ranking_real_time',
+    description='None',
+    author='Hudson Conceição da Silva',
+    author_email='hud90cs@gmail.com',
+    url='',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
