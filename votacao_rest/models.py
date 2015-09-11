761eb31a6fa05a4810ad2ef03b601ad5f967d27a
Hudson Conceição Da Silva <hud90cs@gmail.com>
2015-09-11 17:05:48 -0300

    Adicionando ao admin e ajustando os models

--- votacao_rest/models.py
+++ votacao_rest/models.py
@@ -6,8 +6,14 @@ class Pergunta(models.Model):
     texto_pergunta = models.CharField(max_length=200)
     data_publicacao = models.DateTimeField('Data publicacao')
 
+    def __unicode__(self):
+    	return self.texto_pergunta
+
 
 class Resposta(models.Model):
     pergunta = models.ForeignKey(Pergunta)
     texto_resposta = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
+
+    def __unicode__(self):
+    	return self.texto_resposta
